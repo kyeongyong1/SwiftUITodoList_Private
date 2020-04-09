06d77874279eb129d99ebec9749eeb841c13f2bf
이경용 <lky@igyeong-yong-ui-MacBookPro.local>
2020-04-09 17:26:57 +0900

    init

---
+++ SwiftUITodoListPersonal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITodoListPersonal
+//
+//  Created by 이경용 on 2020/04/09.
+//  Copyright © 2020 이경용. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
